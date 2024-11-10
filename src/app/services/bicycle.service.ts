70271b888d50b142fb5816730b45fa80abe811aa
Orihuela03 <Orihuelamarcos03@gmail.com>
2024-11-10 14:22:32 +0000

    First Commit

---
+++ src/app/services/bicycle.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BicycleService {
+
+  endpoint = 'http://localhost:8080/api/bikes';
+
+  constructor(private httpClient : HttpClient) { }
+
+  getBicycles() {
+    return this.httpClient.get(this.endpoint);
+  }
+}
